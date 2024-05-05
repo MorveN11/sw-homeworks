a55d8313988a05ae5974270aab1f5644d6bb1b32
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-05-05 19:48:39 -0400

    feat: Adding the same project using strategy Design Pattern

--- src/HomeWorks/JeferssonCoronel/May/Fourth/Strategy.cs
+++ src/HomeWorks/JeferssonCoronel/May/Fourth/Strategy.cs
@@ -1,10 +1,103 @@
 namespace HomeWorks.JeferssonCoronel.May.Fourth
 {
-    public class Strategy
+    interface IDataOperations
     {
-        public void Run()
+        void ConnectToDatabase();
+        void SelectDataSource();
+        void ProcessData();
+        void ParseAndFormatData();
+        void SendFormattedData();
+        void DisconnectFromDatabase();
+    }
+
+    class SQLDataOperations : IDataOperations
+    {
+        public void ConnectToDatabase()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Connecting to SQL database...");
+        }
+
+        public void SelectDataSource()
+        {
+            Console.WriteLine("Selecting tables from SQL database...");
+        }
+
+        public void ProcessData()
+        {
+            Console.WriteLine("Processing data from SQL database...");
+        }
+
+        public void ParseAndFormatData() { }
+
+        public void SendFormattedData() { }
+
+        public void DisconnectFromDatabase()
+        {
+            Console.WriteLine("Finalizing the connection with the SQL database...");
+        }
+    }
+
+    class MongoDataOperations : IDataOperations
+    {
+        public void ConnectToDatabase()
+        {
+            Console.WriteLine("Connecting to MongoDB...");
+        }
+
+        public void SelectDataSource()
+        {
+            Console.WriteLine("Selecting collections from MongoDB...");
+        }
+
+        public void ProcessData()
+        {
+            Console.WriteLine("Processing data from MongoDB...");
+        }
+
+        public void ParseAndFormatData()
+        {
+            Console.WriteLine("Parsing and formatting data from MongoDB...");
+        }
+
+        public void SendFormattedData() { }
+
+        public void DisconnectFromDatabase()
+        {
+            Console.WriteLine("Finalizing the connection with the MongoDB database...");
+        }
+    }
+
+    class DataMiner
+    {
+        private IDataOperations dataOperations;
+
+        public DataMiner(IDataOperations dataOperations)
+        {
+            this.dataOperations = dataOperations;
+        }
+
+        public void MineData()
+        {
+            dataOperations.ConnectToDatabase();
+            dataOperations.SelectDataSource();
+            dataOperations.ProcessData();
+            dataOperations.ParseAndFormatData();
+            dataOperations.SendFormattedData();
+            dataOperations.DisconnectFromDatabase();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMiner sqlMiner = new DataMiner(new SQLDataOperations());
+            sqlMiner.MineData();
+
+            Console.WriteLine();
+
+            DataMiner mongoMiner = new DataMiner(new MongoDataOperations());
+            mongoMiner.MineData();
         }
     }
 }
