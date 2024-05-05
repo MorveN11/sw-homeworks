c4d9dff1308a8bd964d9bff74c3561cca21876e5
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-05-05 19:33:15 -0400

    feat: Adding project using template Design Pattern

--- src/HomeWorks/JeferssonCoronel/May/Fourth/Template.cs
+++ src/HomeWorks/JeferssonCoronel/May/Fourth/Template.cs
@@ -1,10 +1,80 @@
-namespace HomeWorks.JeferssonCoronel.May.Fourth
+namespace HomeWorks.JeffersonCoronel.May.Fourth
 {
-    public class Template
-    {
-        public void Run()
+    abstract class DataMiner {
+        public void MineData()
         {
-            Console.WriteLine("Hello World!");
+            ConnectToDatabase();
+            SelectDataSource();
+            ProcessData();
+            ParseAndFormatData();
+            SendFormattedData();
+            DisconnectFromDatabase();
+        }
+
+        protected abstract void ConnectToDatabase();
+        protected abstract void SelectDataSource();
+        protected abstract void ProcessData();
+        protected virtual void ParseAndFormatData() { }
+        protected virtual void SendFormattedData() { }
+
+        protected virtual void DisconnectFromDatabase()
+        {
+            Console.WriteLine("Finalizing the connection with the database ...");
+        }
+    }
+
+    class SQLDataMiner : DataMiner
+    {
+        protected override void ConnectToDatabase()
+        {
+            Console.WriteLine("Connecting to SQL database...");
+        }
+
+        protected override void SelectDataSource()
+        {
+            Console.WriteLine("Selecting tables from SQL database...");
+        }
+
+        protected override void ProcessData()
+        {
+            Console.WriteLine("Processing data from SQL database...");
+        }
+    }
+
+    class MongoDataMiner : DataMiner
+    {
+        protected override void ConnectToDatabase()
+        {
+            Console.WriteLine("Connecting to MongoDB database...");
+        }
+
+        protected override void SelectDataSource()
+        {
+            Console.WriteLine("Selecting collections from MongoDB...");
+        }
+
+        protected override void ProcessData()
+        {
+            Console.WriteLine("Processing data from MongoDB...");
+        }
+
+        protected override void ParseAndFormatData()
+        {
+            Console.WriteLine("Parsing and formatting data from MongoDB...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMiner sqlMiner = new SQLDataMiner();
+            sqlMiner.MineData();
+
+            Console.WriteLine();
+
+            DataMiner mongoMiner = new MongoDataMiner();
+            mongoMiner.MineData();
         }
     }
 }
