36f506b526f526ed8a08890e27bc58dc512396bd
JheremyTancara <jheremykay777@gmail.com>
2024-05-10 08:16:46 -0400

    task pattern designs

---
+++ src/HomeWorks/JheremyTancara/May/Sixth/factory/Factory.md
@@ -0,0 +1,10 @@
+<div style="text-align: center;">
+
+# Factory Pattern</div>
+
+### Author: Jheremy Kevin Tancara Zambrana - 5 A
+--
+
+El patrón de diseño Factory es un patrón creacional que proporciona una interfaz para crear objetos, pero permite que las subclases decidan qué clase instanciar. El patrón Factory permite la creación de objetos sin especificar la clase exacta que se creará.
+
+Existen diferentes variaciones del patrón Factory, como el Simple Factory, Factory Method y Abstract Factory. A continuación, te muestro un ejemplo de implementación en C# utilizando el Factory Method, que es una variación en la que una clase base define un método para crear un objeto, y las subclases especifican qué tipo de objeto crear.
---
+++ src/HomeWorks/JheremyTancara/May/Sixth/factory/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace FactoryMethodPattern
+{
+    abstract class Product
+    {
+        public abstract void Display();
+    }
+
+    class ProductA : Product
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Producto A");
+        }
+    }
+
+    class ProductB : Product
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Producto B");
+        }
+    }
+
+    abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+        public void ShowProduct()
+        {
+            Product product = FactoryMethod();
+            product.Display();
+        }
+    }
+
+    class CreatorA : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ProductA();
+        }
+    }
+
+    class CreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ProductB();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Creator creatorA = new CreatorA();
+            Creator creatorB = new CreatorB();
+
+            Console.WriteLine("Usando CreatorA:");
+            creatorA.ShowProduct();
+
+            Console.WriteLine("\nUsando CreatorB:");
+            creatorB.ShowProduct();
+        }
+    }
+}
