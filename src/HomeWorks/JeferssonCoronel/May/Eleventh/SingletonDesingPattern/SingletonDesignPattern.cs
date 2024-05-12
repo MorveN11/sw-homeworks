22f81d6e935fc0df607acc746974629d31c98565
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-05-12 01:11:46 -0400

    feat: Adding Project of my example aplying Signleton Design Pattern

---
+++ src/HomeWorks/JeferssonCoronel/May/Eleventh/SingletonDesingPattern/SingletonDesignPattern.cs
@@ -0,0 +1,58 @@
+namespace HomeWorks.JeferssonCoronel.May.Eleventh.SingletonDesignPattern
+{
+    public class ConfigurationManager
+    {
+        private static ConfigurationManager instance;
+        private readonly Dictionary<string, string> configData;
+
+        private ConfigurationManager()
+        {
+            configData = new Dictionary<string, string>();
+        }
+
+        public static ConfigurationManager GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new ConfigurationManager();
+            }
+            return instance;
+        }
+
+        public string GetConfigValue(string key)
+        {
+            if (configData.ContainsKey(key))
+            {
+                return configData[key];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void SetConfigValue(string key, string value)
+        {
+            configData[key] = value;
+        }
+    }
+
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var configManager = ConfigurationManager.GetInstance();
+
+            configManager.SetConfigValue("AppLanguage", "English");
+            configManager.SetConfigValue("AppTheme", "Dark");
+
+            Console.WriteLine("AppLanguage: " + configManager.GetConfigValue("AppLanguage"));
+            Console.WriteLine("AppTheme: " + configManager.GetConfigValue("AppTheme") + "\n");
+
+            // This is other variable but has the same instance so their variables have the same values
+            var configManagerNewVariable = ConfigurationManager.GetInstance();
+            Console.WriteLine("AppLanguage: " + configManagerNewVariable.GetConfigValue("AppLanguage"));
+            Console.WriteLine("AppTheme: " + configManagerNewVariable.GetConfigValue("AppTheme"));
+        }
+    }
+}
